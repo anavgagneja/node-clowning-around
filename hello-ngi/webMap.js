6ef315fd01960712df442b467fa239c2773837db
Dhruv Upadhyay <Dhruv@Dhruvs-MacBook-Pro.local>
2016-10-09 06:47:37 -0400

    website navbar

--- hello-ngi/webMap.js
+++ hello-ngi/webMap.js
@@ -3,6 +3,44 @@
       // failed.", it means you probably did not give permission for the browser to
       // locate you.
 
+      var navClicked = true;
+var sections = $('section')
+    , nav = $('nav')
+    , nav_height = nav.outerHeight();
+$(document).ready(function(){       
+
+
+    nav.find('a').on('click', function () {
+      var $el = $(this)
+          , id = $el.attr('href');
+
+      $('html, body').animate({
+          scrollTop: $(id).offset().top - nav_height
+      }, 500);
+
+      return false;
+    });
+
+      $(document).scroll(function(){
+        if($(this).scrollTop() > 40)
+        {
+            $('.navbar-default').css({"background":"#e74c3c"});
+            $('#top').css({"display":"block"});
+            $('.navbar-default').css({" -webkit-box-shadow": "0 4px 4px rgba(0, 0, 0, 0.5)",
+                                        "-moz-box-shadow": "0 4px 4px rgba(0, 0, 0, 0.5)",
+                                         "box-shadow": "0 4px 4px rgba(0, 0, 0, 0.5)"});
+        }  else {
+
+             $('#top').css({"display":"none"});
+           $('.navbar-default').css({"background":"transparent"});
+            $('.navbar-default').css({" -webkit-box-shadow": "0 0px 0px rgba(0, 0, 0, 0.5)",
+                "-moz-box-shadow": "0 0px 0px rgba(0, 0, 0, 0.5)",
+                "box-shadow": "0 0px 0px rgba(0, 0, 0, 0.5)"});
+
+        }
+        });
+    
+});
 
 var stylesArray = [  {    "elementType": "geometry",    "stylers": [      {        "color": "#1d2c4d"      }    ]  },  {    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#8ec3b9"      }    ]  },  {    "elementType": "labels.text.stroke",    "stylers": [      {        "color": "#1a3646"      }    ]  },  {    "featureType": "administrative.country",    "elementType": "geometry.stroke",    "stylers": [      {        "color": "#4b6878"      }    ]  },  {    "featureType": "administrative.land_parcel",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#64779e"      }    ]  },  {    "featureType": "administrative.province",    "elementType": "geometry.stroke",    "stylers": [      {        "color": "#4b6878"      }    ]  },  {    "featureType": "landscape.man_made",    "elementType": "geometry.stroke",    "stylers": [      {        "color": "#334e87"      }    ]  },  {    "featureType": "landscape.natural",    "elementType": "geometry",    "stylers": [      {        "color": "#023e58"      }    ]  },  {    "featureType": "poi",    "elementType": "geometry",    "stylers": [      {        "color": "#283d6a"      }    ]  },  {    "featureType": "poi",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#6f9ba5"      }    ]  },  {    "featureType": "poi",    "elementType": "labels.text.stroke",    "stylers": [      {        "color": "#1d2c4d"      }    ]  },  {    "featureType": "poi.park",    "elementType": "geometry.fill",    "stylers": [      {        "color": "#023e58"      }    ]  },  {    "featureType": "poi.park",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#3C7680"      }    ]  },  {    "featureType": "road",    "elementType": "geometry",    "stylers": [      {        "color": "#304a7d"      }    ]  },  {    "featureType": "road",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#98a5be"      }    ]  },  {    "featureType": "road",    "elementType": "labels.text.stroke",    "stylers": [      {        "color": "#1d2c4d"      }    ]  },  {    "featureType": "road.highway",    "elementType": "geometry",    "stylers": [      {        "color": "#2c6675"      }    ]  },  {    "featureType": "road.highway",    "elementType": "geometry.stroke",    "stylers": [      {        "color": "#255763"      }    ]  },  {    "featureType": "road.highway",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#b0d5ce"      }    ]  },  {    "featureType": "road.highway",    "elementType": "labels.text.stroke",    "stylers": [      {        "color": "#023e58"      }    ]  },  {    "featureType": "transit",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#98a5be"      }    ]  },  {    "featureType": "transit",    "elementType": "labels.text.stroke",    "stylers": [      {        "color": "#1d2c4d"      }    ]  },  {    "featureType": "transit.line",    "elementType": "geometry.fill",    "stylers": [      {        "color": "#283d6a"      }    ]  },  {    "featureType": "transit.station",    "elementType": "geometry",    "stylers": [      {        "color": "#3a4762"      }    ]  },  {    "featureType": "water",    "elementType": "geometry",    "stylers": [      {        "color": "#0e1626"      }    ]  },  {    "featureType": "water",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#4e6d70"      }    ]  }];
 var firebaseRef = firebase.database().ref();
